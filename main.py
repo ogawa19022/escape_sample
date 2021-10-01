97797c433424f2d31acbd326d143b605d4ddece3
ogawa19022 <t19j022ok@ous.jp>
2021-10-01 15:07:14 +0900

    create new project

---
+++ main.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+import sys
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((750,563))
+    clock = pygame.time.Clock()
+    
+    img_bg = pygame.image.load("bg.png")
+    img_it = pygame.image.load("item.png")
+    img_it_on = pygame.image.load("item_target.png")
+
+    pic_st = 0
+    
+    while True:
+
+        """ イベント """
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if pic_st == 1:
+                    pic_st = 2
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        """ マウスカーソル位置捕捉 """
+        x, y = pygame.mouse.get_pos()
+        if pic_st != 2:
+            if 494 <= x <= 532 and 144 <= y <= 185:
+                pic_st = 1
+            else:
+                pic_st = 0
+        
+        """ 背景・ブロック描画 """
+        screen.blit(img_bg, [0, 0])
+        if pic_st == 0:
+            screen.blit(img_it, [494, 144])
+        elif pic_st == 1:
+            screen.blit(img_it_on, [494, 144])
+        pygame.display.update()
+        
+        clock.tick(30)
+        
+        
+if __name__ == '__main__':
+    main()
